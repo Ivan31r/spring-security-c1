4b098ab173adbe515ab4898349a21b0ced035e69
IUsov <ivan-vanes007@rambler.ru>
2023-09-19 16:45:46 +0300

    Initial commit

---
+++ src/main/java/com/laurspilca/spring_security_c1/SpringSecurityC1Application.java
@@ -0,0 +1,13 @@
+package com.laurspilca.spring_security_c1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityC1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityC1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/laurspilca/spring_security_c1/SpringSecurityC1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurspilca.spring_security_c1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityC1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
