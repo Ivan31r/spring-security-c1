e7981f958068215baa65d4699c9a476f33ca18d7
IUsov <ivan-vanes007@rambler.ru>
2023-09-21 18:03:12 +0300

    custom userDetailManager and passwordEncoder

---
+++ spring-security-l3/src/test/java/com/example/springsecurityl3/SpringSecurityL3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityl3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityL3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
