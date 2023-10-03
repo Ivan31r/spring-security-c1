1dc9eecd27eb1fdfe607565eed09b07db7372240
IUsov <ivan.usov2011@yandex.ru>
2023-10-03 15:26:11 +0300

    oauth 2 - understanding 4 types of grand_types

--- spring-security-l12/src/main/java/com/example/springsecurityl12/config/AuthServerConfig.java
+++ spring-security-l12/src/main/java/com/example/springsecurityl12/config/AuthServerConfig.java
@@ -1,12 +1,16 @@
 package com.example.springsecurityl12.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -30,14 +34,29 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
                 .withClient("client1")
                 .secret("secret1")
                 .scopes("read")
-                .authorizedGrantTypes("password")
+                .authorizedGrantTypes("password", "refresh_token")
                 .and()
                 .withClient("client2")
                 .secret("secret2")
                 .scopes("read")
                 .authorizedGrantTypes("authorization_code")
-                .redirectUris("http://localhost:9090");
+                .redirectUris("http://localhost:9090")
+                .and()
+                .withClient("client3")
+                .secret("secret3")
+                .scopes("read", "write")
+                .authorizedGrantTypes("client_credentials");
     }
+//просто способю смены обычных токенов на JWT
+//    @Bean
+//    public TokenStore tokenStore(){
+//        return new JwtTokenStore(converter());
+//    }
+//
+//    @Bean
+//    public JwtAccessTokenConverter converter(){
+//        return new JwtAccessTokenConverter();
+//    }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
