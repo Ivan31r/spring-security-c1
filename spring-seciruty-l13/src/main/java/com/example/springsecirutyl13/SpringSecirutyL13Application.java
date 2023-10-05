3331c9c39c8bc4d35d940ee86f44ebac5fcb6392
IUsov <ivan-vanes007@rambler.ru>
2023-10-05 17:52:54 +0300

    understanding 2 different token types

---
+++ spring-seciruty-l13/src/main/java/com/example/springsecirutyl13/SpringSecirutyL13Application.java
@@ -0,0 +1,13 @@
+package com.example.springsecirutyl13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecirutyL13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecirutyL13Application.class, args);
+    }
+
+}
---
+++ spring-seciruty-l13/src/test/java/com/example/springsecirutyl13/SpringSecirutyL13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecirutyl13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecirutyL13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
