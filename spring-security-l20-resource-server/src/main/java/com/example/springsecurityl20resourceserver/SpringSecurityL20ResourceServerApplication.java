a85cd13fb2a0c811d0716bf8ebef2027204394cb
IUsov <ivan.usov2011@yandex.ru>
2023-10-17 08:10:06 +0300

    use keycloak as auth server

---
+++ spring-security-l20-resource-server/src/main/java/com/example/springsecurityl20resourceserver/SpringSecurityL20ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityl20resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityL20ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityL20ResourceServerApplication.class, args);
+    }
+
+}
---
+++ spring-security-l20-resource-server/src/test/java/com/example/springsecurityl20resourceserver/SpringSecurityL20ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityl20resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityL20ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
